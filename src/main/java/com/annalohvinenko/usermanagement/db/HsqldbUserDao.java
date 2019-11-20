9e6cd1242e2439f9fcd8a5bd8bb9ef15a4900457
annalohvinenko1 <anna.lohvinenko@nure.ua>
2019-11-21 00:24:57 +0200

    We did the step 1: Создание классов доступа к данным в БД

---
+++ src/main/java/com/annalohvinenko/usermanagement/db/HsqldbUserDao.java
@@ -0,0 +1,43 @@
+package com.annalohvinenko.usermanagement.db;
+
+import java.util.Collection;
+
+import com.annalohvinenko.usermanagement.User;
+
+public class HsqldbUserDao implements UserDao {
+
+	public HsqldbUserDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public User create(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User find(long id) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<User> findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User user) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/annalohvinenko/usermanagement/db/UserDao.java
@@ -0,0 +1,21 @@
+package com.annalohvinenko.usermanagement.db;
+
+import java.util.Collection;
+
+import com.annalohvinenko.usermanagement.User;
+
+import java.util.Collection;
+
+public interface UserDao {
+    User create(User user) throws DatabaseException;
+
+    User find(long id) throws DatabaseException;
+
+    Collection<User> findAll() throws DatabaseException;
+
+    void update (User user) throws DatabaseException;
+
+    void delete (User user) throws DatabaseException;
+
+ 
+}
