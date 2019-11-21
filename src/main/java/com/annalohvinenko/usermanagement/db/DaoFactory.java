1112555753d574d14306e24b4aca5995b73c46f5
annalohvinenko1 <anna.lohvinenko@nure.ua>
2019-11-21 17:27:18 +0200

    We finalized the ConnectionFactoryImp to working with the parameter file

---
+++ src/main/java/com/annalohvinenko/usermanagement/db/DaoFactory.java
@@ -0,0 +1,33 @@
+package com.annalohvinenko.usermanagement.db;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class DaoFactory {
+	private static final String USER_DAO = "dao.com.markopavlenko.usermanagement.db.UserDao";
+    private static final String CONNECTION_DRIVER = "connection.driver";
+    private static final String CONNECTION_URL = "connection.url";
+    private static final String CONNECTION_USER = "connection.user";
+    private static final String CONNECTION_PASSWORD = "connection.password";
+    private static final String PROPERTIES_FILE = "settings.properties";
+   
+    private Properties properties;
+
+    private DaoFactory() {
+        properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    
+    private ConnectionFactory getConnectionFactory() {
+        String driver = properties.getProperty(CONNECTION_DRIVER);
+        String url = properties.getProperty(CONNECTION_URL);
+        String user = properties.getProperty(CONNECTION_USER);
+        String password = properties.getProperty(CONNECTION_PASSWORD);
+        return new ConnectionFactoryImpl(driver, url, user, password);
+    }
+}
