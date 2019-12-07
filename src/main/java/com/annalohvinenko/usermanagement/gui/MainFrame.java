6089388ad37cd64c63602bbbeb1c0421f2799a84
annalohvinenko1 <anna.lohvinenko@nure.ua>
2019-12-07 16:56:03 +0300

    We created and begin to fulled MainFrame & MainFrameTest files

---
+++ src/main/java/com/annalohvinenko/usermanagement/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package com.annalohvinenko.usermanagement.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/annalohvinenko/usermanagement/gui/MainFrameTest.java
@@ -0,0 +1,62 @@
+package com.annalohvinenko.usermanagement.gui;
+
+import com.annalohvinenko.usermanagement.User;
+import com.annalohvinenko.usermanagement.db.DaoFactory;
+
+
+import com.mockobjects.dynamic.Mock;
+import junit.extensions.jfcunit.JFCTestCase;
+import junit.extensions.jfcunit.JFCTestHelper;
+import junit.extensions.jfcunit.TestHelper;
+
+import java.util.ArrayList;
+
+import java.util.Properties;
+
+public class MainFrameTest extends JFCTestCase {
+ 
+
+    private MainFrame mainFrame;
+    private Mock mockUserDao;
+    private ArrayList<User> users;
+
+    @Override
+    protected void setUp() throws Exception {
+        // TODO Auto-generated method stub
+        super.setUp();
+        try {
+            Properties properties = new Properties();
+
+
+            DaoFactory.init(properties);
+
+
+           
+
+            users = new ArrayList<User>();
+           
+
+     
+            setHelper(new JFCTestHelper());
+            mainFrame = new MainFrame();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        mainFrame.setVisible(true);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        try {
+            super.tearDown();
+            mockUserDao.verify();
+//				mainFrame.setVisible(false);
+            getHelper();
+            TestHelper.cleanUp(this);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }//done
+
+   
+}
